c9f2fff7995e44edb7121e0450ec67ca66a67f52
Dmitriy <dmitriy.shehobalov@gmail.com>
2022-11-04 19:11:13 +0300

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 10:
+// Напишите программу которая принимает на вход трехзначное числа 
+// и на выходе показывает вторую цифру этого числа
+
+int num = new Random().Next(100, 1000);
+
+int SecondNumberMethod(int arg1)
+{
+    int thirdDigit = num / 10;
+    int secondDigit = thirdDigit % 10;
+    return arg1 = secondDigit;
+}
+int result = SecondNumberMethod(num);
+Console.WriteLine($"Second number {num} is a {result}");
+
